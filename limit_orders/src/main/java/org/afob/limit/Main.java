f9eb44992271e924f0baafb7aa843ffd7033e8d6
Shivam Dhawan <Shivam.Dhawan@in.ey.com>
2024-08-23 13:48:01 +0530

    added main file

--- limit_orders/src/main/java/org/afob/limit/Main.java
+++ limit_orders/src/main/java/org/afob/limit/Main.java
@@ -8,10 +8,8 @@ public class Main {
     public static void main(String[] args) {
         ExecutionClient executionClient = new ExecutionClient();
         LimitOrderAgent limitOrderAgent = new LimitOrderAgent(executionClient);
-
         limitOrderAgent.addOrder(true, "IBM", 1000, BigDecimal.valueOf(100));
         limitOrderAgent.addOrder(false, "IBM", 500, BigDecimal.valueOf(120));
-
         limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(90)); // Should trigger the buy order
         limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(130)); // Should trigger the sell order
     }
